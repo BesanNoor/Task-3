fceafdb4fbebb469c76e034a0a17a7ecce70bafd
BesanNoor <sfbeno520@gmail.com>
2025-11-01 20:56:51 +0200

    Update thtask.js

--- thtask.js
+++ thtask.js
@@ -1,6 +1,6 @@
 //ביסאם פרח 211861208
 //נור צובח 214459463
-
+//תוכנית המגדירה מערך של מספרים שלמים ומחשבת כמות אפסים
 const arr = [0, 3, -2, 0, 4, 0, 1];
 let count = 0;
 
@@ -9,3 +9,4 @@ for (let i = 0; i < arr.length; i++) {
 }
 
 console.log("The amount of zeros is: ", count);
+
